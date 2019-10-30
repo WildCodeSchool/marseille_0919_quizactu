f2d29eaa5d661427396adf68fdef5ff6e51ac896
Nin1963 <ninayamina1963@gmail.com>
2019-10-30 16:19:28 +0100

    Add login page

--- src/main/java/fr/actuz/quizactu/business/entity/Account.java
+++ src/main/java/fr/actuz/quizactu/business/entity/Account.java
@@ -72,4 +72,62 @@ public class Account implements Serializable {
 	@JoinTable(name = "ACCOUNT_has_ARTICLE")
 	private List<Article> articles;
 
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Integer getScore() {
+		return score;
+	}
+
+	public void setScore(Integer score) {
+		this.score = score;
+	}
+
+	public List<Article> getArticles() {
+		return articles;
+	}
+
+	public void setArticles(List<Article> articles) {
+		this.articles = articles;
+	}
+	
+	
+
 }
--- src/main/java/fr/actuz/quizactu/controller/IndexController.java
+++ src/main/java/fr/actuz/quizactu/controller/IndexController.java
@@ -1,12 +1,22 @@
 package fr.actuz.quizactu.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import fr.actuz.quizactu.business.entity.Account;
 
 @Controller
 public class IndexController {
 	@RequestMapping("/")
 	public String index() {
 		return "index";
+	}
+	
+	@GetMapping("/connection")
+	public String connection(Model model) {
+		model.addAttribute("userForm", new Account());
+		return "login";
 	}
 }

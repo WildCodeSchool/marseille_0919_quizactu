c8a5b4300664f4b1a03068c0096b355da7eaaaef
Nin1963 <ninayamina1963@gmail.com>
2019-11-14 09:24:03 +0100

    Mise en place session utilisateur

--- src/main/java/fr/actuz/quizactu/controller/QuizController.java
+++ src/main/java/fr/actuz/quizactu/controller/QuizController.java
@@ -1,36 +1,68 @@
 package fr.actuz.quizactu.controller;
 
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
+import fr.actuz.quizactu.business.entity.Account;
 import fr.actuz.quizactu.business.entity.Quiz;
+import fr.actuz.quizactu.business.service.AccountService;
 import fr.actuz.quizactu.business.service.QuizService;
 
 @Controller
+@SessionAttributes({"accountId", "quiz", "questionIndex"})
+@Scope("session")
 public class QuizController {
 
 	@Autowired
 	private QuizService service;
-
-	private Quiz quiz;
-
-	private Integer index;
+	
+	@Autowired
+	private AccountService accountService;
+	
+	@ModelAttribute("accountId")
+	public Integer account() {
+		return null;
+	}
+	
+	@ModelAttribute("quiz")
+	public Quiz quiz() {
+		return null;
+	}
+	
+	@ModelAttribute("questionIndex")
+	public Integer question() {
+		return null;
+	}
 
 	@GetMapping("/quiz")
-	public String vuQuestion(Model model) {
-		this.quiz = this.service.getQuizByPublicationDate();
-		this.index = 0;
-		model.addAttribute("question", this.quiz.getQuestions().get(index));
+	public String vuQuestion(Model model, 
+			@ModelAttribute("accountId") Integer accountId,
+			Principal principal) {
+		Quiz quiz = this.service.getQuizByPublicationDate();
+		int index = 0;
+		model.addAttribute("quiz", quiz);
+		model.addAttribute("question", quiz.getQuestions().get(index));
+		model.addAttribute("questionIndex", index);
+		if(accountId == null) {
+			Account account = this.accountService.read(principal.getName());
+			model.addAttribute("accountId", account.getId());
+		}
 		return "quiz";
+		
 	}
 
 	@GetMapping("nextQuestion/{questionId}/{responseId}")
-	public String nextQuestion(Model model, Integer questionId, Integer responseId) {
+	public String nextQuestion(Model model, Integer questionId, Integer responseId, @ModelAttribute("quiz") Quiz quiz, @ModelAttribute("questionIndex") int index) {
 		// TODO: Enregistrer la réponse choisie en BDD.
 		if (index < quiz.getQuestions().size() - 1) {
-			model.addAttribute("question", this.quiz.getQuestions().get(++index));
+			model.addAttribute("question", quiz.getQuestions().get(++index));
 			
 			return "quiz";
 		} else {

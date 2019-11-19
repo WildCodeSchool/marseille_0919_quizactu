0a61129c3d4c9b18b8aef59416c099db61a58a2f
Abdelkrim <abdel.barkaoui@gmail.com>
2019-11-19 11:57:11 +0100

    petit reglage

--- src/main/java/fr/actuz/quizactu/business/service/QuizRecordService.java
+++ src/main/java/fr/actuz/quizactu/business/service/QuizRecordService.java
@@ -13,24 +13,23 @@ import fr.actuz.quizactu.business.entity.QuizRecord;
 import fr.actuz.quizactu.business.entity.Response;
 import fr.actuz.quizactu.persistence.AccountRepository;
 import fr.actuz.quizactu.persistence.QuizRecordRepository;
-import fr.actuz.quizactu.persistence.QuizRepository;
 
 @Service
 public class QuizRecordService {
-	
+
 	@Autowired
 	private QuizRecordRepository recordRepo;
-	
+
 	@Autowired
 	private QuizService quizService;
-	
+
 	@Autowired
 	private AccountRepository accountRepo;
-	
-	// Save result quiz 
+
+	// Save result quiz
 	public void recordResultQuiz(Integer quizId, Integer responseId, Integer accountId) {
-		Quiz quiz = quizService.getQuizById(quizId);
-		Response resp = quizService.getResponseById(responseId);
+		Quiz quiz = this.quizService.getQuizById(quizId);
+		Response resp = this.quizService.getResponseById(responseId);
 		Account acc = this.accountRepo.getOne(accountId);
 		QuizRecord quizRecord = new QuizRecord(quiz, resp, acc);
 		this.recordRepo.save(quizRecord);
@@ -41,42 +40,40 @@ public class QuizRecordService {
 		List<QuizRecord> quizRecord = this.recordRepo.findAllByQuizIdAndAccountId(quizId, accountId);
 		Integer score = 0;
 		for (QuizRecord result : quizRecord) {
-			if(result.getResponse().getIsTrue()) {
+			if (result.getResponse().getIsTrue()) {
 				score += 10;
 			}
 		}
 		return score;
 	}
-	
+
 	public List<Response> getQuizResponses(Integer quizId, Integer accountId) {
 		List<QuizRecord> quizRecord = this.recordRepo.findAllByQuizIdAndAccountId(quizId, accountId);
-		List<Response> responses = new ArrayList<Response>();
-		
+		List<Response> responses = new ArrayList<>();
+
 		for (QuizRecord result : quizRecord) {
 			responses.add(result.getResponse());
 		}
-		
+
 		return responses;
 	}
-	
+
 	public List<QuizRecord> getByQuizIdAndAccountId(Integer quizId, Integer accountId) {
 		List<QuizRecord> quizRecord = this.recordRepo.findAllByQuizIdAndAccountId(quizId, accountId);
 		return quizRecord;
 	}
-	
-	public boolean compareIfQuestionAlreadyAnswered(Integer quizId, Integer accountId, Integer responseId, Question curQuestion) {
+
+	public boolean compareIfQuestionAlreadyAnswered(Integer quizId, Integer accountId, Integer responseId,
+			Question curQuestion) {
 		List<QuizRecord> records = this.recordRepo.findAllByQuizIdAndAccountId(quizId, accountId);
 		Response responseRecord;
 		for (QuizRecord quizRecord : records) {
-			responseRecord = quizService.getResponseById(quizRecord.getResponse().getId());
-			if(responseRecord.getQuestion().getId() == curQuestion.getId()) {
-				System.out.println("c'est égal");
+			responseRecord = this.quizService.getResponseById(quizRecord.getResponse().getId());
+			if (responseRecord.getQuestion().getId() == curQuestion.getId()) {
 				return false;
 			}
 		}
 		return true;
 	}
-	
-	
-	
+
 }

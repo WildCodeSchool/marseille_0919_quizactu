2aed065307fa0788b7ba8bc289b33c9f06fd13a1
Jérémy Masson <jeremy.masson@wildcodeschool.fr>
2019-11-26 14:01:01 +0100

    Correction accès sécurisés USER/MANAGER + remise en forme et commenaires.

--- src/main/java/fr/actuz/quizactu/config/SecurityConfig.java
+++ src/main/java/fr/actuz/quizactu/config/SecurityConfig.java
@@ -12,15 +12,27 @@ import org.springframework.security.web.access.expression.DefaultWebSecurityExpr
 
 @Configuration
 @EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().expressionHandler(this.webExpressionHandler())
-				.antMatchers("/public/**", "/webjars/**", "/images/**", "/css/**").permitAll().anyRequest()
-				.authenticated().antMatchers("/manager").hasAuthority("MANAGER").and().formLogin().permitAll()
-				.loginPage("/login").successHandler(new LoginHandler()).and().logout().logoutSuccessUrl("/disconnected")
-				.permitAll();
+				// Paramétrage des accès aux URL de création de quiz seulement pour les profiles
+				// utilisateurs avec le role MANAGER.
+				.antMatchers("/manager/**").hasAuthority("MANAGER")
+				// Paramétrage des accès publiques anonymes (ne nécessite pas d'être connecté.
+				.antMatchers("/public/**", "/webjars/**", "/images/**", "/css/**").permitAll()
+				// Paramétrage de toutes les autres URL (excepté celles décrites au dessus)
+				// seulement pour les utilisateurs authentifiés.
+				.anyRequest().authenticated()
+				// Paramétrage du moyen d'authentification en formulaire. Success handler pour
+				// gérer
+				// vers quelle URL rediriger après une authentification valide.
+				.and().formLogin().permitAll().loginPage("/login").successHandler(new LoginHandler())
+				// Paramétrage du moyen de déconnexion avec redirection vers une route spéciale
+				// pour gérer les attributs de session.
+				.and().logout().logoutSuccessUrl("/disconnected").permitAll();
 	}
 
 	@Bean

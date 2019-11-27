4b4c0388d18103202ee847ed5b527ff8a8a7e8c4
Jérémy Masson <jeremy.masson@wildcodeschool.fr>
2019-11-27 13:53:13 +0100

    Correction filtre USER sur le classement. Début correction affichage des quiz avec image de la première question (ou logo).

--- src/main/java/fr/actuz/quizactu/persistence/AccountRepository.java
+++ src/main/java/fr/actuz/quizactu/persistence/AccountRepository.java
@@ -1,27 +1,25 @@
 package fr.actuz.quizactu.persistence;
 
-import java.util.*;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import fr.actuz.quizactu.business.entity.Account;
 
-
 /**
- * 
+ *
  */
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Integer> {
 
 	Account findOneByUserName(final String userName);
-	
-	// Find user and score order by score
-	List<Account> findTop10ByOrderByScoreDesc();
-	
-	// Find all user on page ranking;
-	List<Account> findByOrderByScoreDesc();
 
+	// Find user and score order by score
+	List<Account> findTop10ByRoleNameOrderByScoreDesc(String name);
+
+	// Find all user on page ranking;
+	List<Account> findByRoleNameOrderByScoreDesc(String name);
 
 }
